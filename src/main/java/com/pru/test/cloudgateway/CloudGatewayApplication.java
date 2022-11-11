6c0a9813562bc124ce9fd643fb6e653d335d4a9b
raghugit01 <biswalraghunath4@gmail.com>
2022-11-11 20:02:38 +0530

    reading config service
    reading config service

--- src/main/java/com/pru/test/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/pru/test/cloudgateway/CloudGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.pru.test.cloudgateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,6 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class CloudGatewayApplication {
+	
+	@Value("${skill.service.url}")
+	private String skillServiceUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
